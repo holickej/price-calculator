e76836428bd7b9714177b8b0d5759b0d37ade70c
Josef Jan Holický <josefjan@holicky.dev>
2024-04-17 19:59:12 +0200

    Kompletní aplikace

--- index.php
+++ index.php
@@ -11,10 +11,8 @@
         <title>HOLICKY DEVELOPER</title>
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-        <link rel="stylesheet" href="assets/css/price.css">
-        <link rel="stylesheet" href="assets/css/price-calculator.css">
+        <link rel="stylesheet" href="assets/css/style.css">
         
     </head>
     <body>
@@ -23,7 +21,6 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <script src="assets/js/price.js"></script>
-        <script src="assets/js/price-calculator.js"></script>
+        <script src="assets/js/script.js"></script>
     </body>
 </html>
